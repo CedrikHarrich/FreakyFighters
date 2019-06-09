4a02ec32ac809701493e883dae1a93a4476fe720
Cedrik Harrich <cedrik.harrich@outlook.com>
2019-06-09 19:15:29 +0200

    Changed the Game file and added a file for global Constants.

--- server/Game.ts
+++ server/Game.ts
@@ -1,15 +1,13 @@
 class Game {
-    gridHeight : number = 25;
-    gridWidth : number = 25;
     grid : Block[][];
     playerOne : Player;
     playerTwo : Player;
-    canvasHeight : number;
-    canvasWidth : number;
     background : File; 
    
-    constructor(playerOne : Player, playerTwo : Player, background? : File){
+    constructor(playerOne : Player, playerTwo : Player, background : File, grid : Block[][]){
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
+        this.grid = grid;
+        this.background = background;
     }
 }
---
+++ server/GlobalConstants.ts
@@ -0,0 +1,12 @@
+//Pixels of a Block.
+const blockHeight : number = 32;
+const blockWidth : number  = 32;
+
+//Number of Blocks on the grid.
+const gridHeight : number = 25;
+const gridWidth : number = 25;
+
+//Pixels of the Canvas.
+const canvasHeight : number = blockHeight * gridHeight;
+const canvasWidth : number= blockWidth * blockWidth;
+
