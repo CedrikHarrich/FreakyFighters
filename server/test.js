3b6a9648beb5f1cb9a866e5e7a8cc71b210ebfef
Cedrik Harrich <cedrik.harrich@outlook.com>
2019-06-11 00:33:43 +0200

    First prototype. No collision detection yet. 3 Layers: Background, blocks and chars.

--- server/test.js
+++ server/test.js
@@ -9,11 +9,15 @@ background.src = "./assets/background/testBackground.jpg";
 character1.src = "./assets/blocks/blobLeft.png";
 character2.src = "./assets/blocks/blobRight.png";
 block.src = "./assets/blocks/dirtBlock.png";
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext('2d');
+var canvas1 = document.getElementById("layer1");
+var context1 = canvas1.getContext('2d');
+var canvas2 = document.getElementById("layer2");
+var context2 = canvas2.getContext('2d');
+var canvas3 = document.getElementById("layer3");
+var context3 = canvas3.getContext('2d');
 //draw the background
 background.onload = function () {
-    context.drawImage(background, 0, 0);
+    context1.drawImage(background, 0, 0);
 };
 //draw the blocks
 var grid = [
@@ -49,19 +53,46 @@ function blockDrawer(grid) {
     for (var i = 0; i < grid.length; i++) {
         for (var j = 0; j < grid.length; j++) {
             if (grid[i][j] === 1) {
-                context.drawImage(block, x * j, x * i);
+                context2.drawImage(block, x * j, x * i);
             }
         }
     }
 }
 //draw the initial characters
 character1.onload = function () {
-    context.drawImage(character1, 10 * x, 10 * x);
+    context3.drawImage(character1, 10 * x, 10 * x);
 };
 character2.onload = function () {
-    context.drawImage(character2, 14 * x, 10 * x);
+    context3.drawImage(character2, 14 * x, 10 * x);
 };
 block.onload = function () {
     blockDrawer(grid);
 };
+var positionX = 14 * x;
+var positionY = 10 * x;
+document.addEventListener("keydown", function (e) {
+    console.log(e.key);
+    if (e.key === "ArrowLeft") {
+        console.log("du");
+        //context.clearRect(0,0,canvas.width, canvas.height);
+        positionX -= 5;
+        context3.clearRect(0, 0, 800, 800);
+        context3.drawImage(character1, positionX, positionY);
+    }
+    if (e.key === "ArrowRight") {
+        positionX += 5;
+        context3.clearRect(0, 0, 800, 800);
+        context3.drawImage(character2, positionX, positionY);
+    }
+    if (e.key === "ArrowDown") {
+        positionY += 5;
+        context3.clearRect(0, 0, 800, 800);
+        context3.drawImage(character2, positionX, positionY);
+    }
+    if (e.key === "ArrowUp") {
+        positionY -= 5;
+        context3.clearRect(0, 0, 800, 800);
+        context3.drawImage(character2, positionX, positionY);
+    }
+});
 //var game = new Game();
--- server/test.ts
+++ server/test.ts
@@ -12,13 +12,19 @@ character1.src = "./assets/blocks/blobLeft.png";
 character2.src = "./assets/blocks/blobRight.png";
 block.src ="./assets/blocks/dirtBlock.png";
 
-var canvas  = <HTMLCanvasElement> document.getElementById("myCanvas"); 
-var context = canvas.getContext('2d');
+var canvas1  = <HTMLCanvasElement> document.getElementById("layer1"); 
+var context1 = canvas1.getContext('2d');
+
+var canvas2  = <HTMLCanvasElement> document.getElementById("layer2"); 
+var context2 = canvas2.getContext('2d');
+
+var canvas3  = <HTMLCanvasElement> document.getElementById("layer3"); 
+var context3 = canvas3.getContext('2d');
        
 
 //draw the background
 background.onload = function() {
-    context.drawImage(background,0,0);
+    context1.drawImage(background,0,0);
 }
 
 //draw the blocks
@@ -59,7 +65,7 @@ function blockDrawer(grid : number[][]) : void{
         for (let j : number = 0; j < grid.length; j++){
              if (grid[i][j] === 1){
                 
-                context.drawImage(block, x * j, x * i);
+                context2.drawImage(block, x * j, x * i);
              }
         }
     }
@@ -69,14 +75,43 @@ function blockDrawer(grid : number[][]) : void{
 
 //draw the initial characters
 character1.onload = function() {
-    context.drawImage(character1, 10 * x, 10 * x); 
+    context3.drawImage(character1, 10 * x, 10 * x); 
 };
 character2.onload = function() {
-    context.drawImage(character2, 14 * x, 10 * x); 
+    context3.drawImage(character2, 14 * x, 10 * x); 
 };
 
 block.onload = function() {
     blockDrawer(grid);
 }
+
+var positionX = 14 * x;
+var positionY = 10 * x;
+document.addEventListener("keydown", e =>{
+    console.log(e.key);
+    if (e.key === "ArrowLeft"){
+        console.log("du");
+        //context.clearRect(0,0,canvas.width, canvas.height);
+        positionX -=5;
+        context3.clearRect(0,0, 800,800);
+        context3.drawImage(character1, positionX, positionY); 
+      
+    } 
+    if (e.key === "ArrowRight"){
+        positionX +=5
+        context3.clearRect(0,0, 800,800);
+        context3.drawImage(character2, positionX, positionY); 
+    } 
+    if (e.key === "ArrowDown"){
+        positionY += 5;
+        context3.clearRect(0,0, 800,800);
+        context3.drawImage(character2, positionX, positionY); 
+    } 
+    if (e.key === "ArrowUp"){
+        positionY -= 5;
+        context3.clearRect(0,0, 800,800);
+        context3.drawImage(character2, positionX, positionY); 
+    } 
+});
 
 //var game = new Game();
