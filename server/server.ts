502a51afb64317dc86aa0872c9c6a9e08b714503
Cedrik Harrich <cedrik.harrich@outlook.com>
2019-06-15 15:02:33 +0200

    Feature: SetBackGame implemented. Comments added to functions of Server.

--- server/server.ts
+++ server/server.ts
@@ -13,18 +13,20 @@ export class Server {
   private socket: any;
   private users:number = 0;
   private port:number = 3000;
-  private Game:Game;
+  private game:Game;
   private maxClients:number = 2;
   private isReady: boolean = false;  
   private clients: Array<any> = [];
 
+  //When the game starts it should setup a server and
+  //should start listening to all events that can occur.
   constructor(){
     this.init();
     this.registerEvents();
   }
 
   //Initialized the needed settings for setting up a server.
-  //This server is now listening to the port 3000
+  //The server is now listening to the port set in GlobalConstants.
   init(){
     this.app.set("port", this.port);
     this.app.use(express.static('dist'));
@@ -32,37 +34,58 @@ export class Server {
     this.http = require("http").Server(this.app);
     this.io = require("socket.io")(this.http);
 
+    console.log("A server has been created.");
+
     this.server = this.http.listen(this.port, () => {
-        console.log("listening on *:" + this.port);
+        console.log(`The server is now listening on port *:" + ${this.port}.`);
     })
   }
 
+  //The server starts registrating all the events that can occur.
   registerEvents(){
+    //Event: Client connects to the server.
     this.io.on("connection", (socket: any) => {
 
         this.socket = socket;
         this.addClient(socket.id);
 
-        console.log("a user connected");
+        console.log(`The user with ID ${socket.id} has connected.`);
 
         this.userCountHandler();
-
+        //Event: Client disconnects from the server.
         socket.on('disconnect', () => {
-          console.log('disconnected');
+          console.log(`The player with the ID ${socket.id} has disconnected.`);
           this.removeClient(socket.id);
           this.userCountHandler();
         });
 
+        //Event: Client sends a message.
         socket.on("message", function(message: any) {
             let newMessage = "Other Person: " + message;
 
             socket.broadcast.emit("message", newMessage);
+
+            console.log(`The player ${socket.id} has sent the message '${message}'.`);
         });
 
+        //Event: Client presses a key.
         socket.on("keyup", function(event: any) {
-            console.log(event);
+            console.log(`${socket.id} has pushed the '${event}' button.`);
+            
         });
-    })
+    });
+
+  }
+
+  setBackGame(){
+    //Disconnect all the clients.
+    for (let entry of this.clients){
+      this.removeClient(entry.id);
+      console.log(`${entry.id} has been kicked.`);
+    }
+    //Delete the old game.
+    this.game = null;
+    console.log(`The game has been set back. New players can join again.`);
   }
 
   addClient(id:number){
@@ -80,10 +103,9 @@ export class Server {
   userCountHandler(){
     if(this.clients.length == this.maxClients){
       this.startGame();
-      console.log('new games')
     }
     else {
-      this.Game = null;
+      this.game = null;
 
       if(this.clients.length < this.maxClients){
         // notifies specific client
@@ -93,7 +115,7 @@ export class Server {
         this.io.emit("message", "Too many Clients. Please sign off.");
       }
     }
-    console.log(this.clients);
+    //console.log(this.clients);
   }
 
   sendGame(){
@@ -101,8 +123,10 @@ export class Server {
   }
 
   startGame(){
-    this.Game = new Game();
-    let players:[Player, Player] = this.Game.getPlayers();
+    this.game = new Game();
+    this.io.emit("message", "Game has started.");
+    console.log(`A new game has started.`);
+    let players:[Player, Player] = this.game.getPlayers();
 
     if (players.length === this.clients.length){
       for(let i = 0; i < this.clients.length; i++){
@@ -111,7 +135,7 @@ export class Server {
     }
 
     // should eventually emit gamestate instead of players
-    this.io.emit("gameStarts", this.Game.getPlayers());
+    this.io.emit("gameStarts", this.game.getPlayers());
   }
 
   updateGame(){
