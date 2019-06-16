63c3a2f7c35b2ec733b24a4d4126d13f9b4b0db2
Cedrik Harrich <cedrik.harrich@outlook.com>
2019-06-16 16:47:37 +0200

    Added some Constants to GlobalConstants

--- server/GlobalConstants.ts
+++ server/GlobalConstants.ts
@@ -1,13 +1,27 @@
 export class GlobalConstants {
-  static readonly CANVAS_HEIGHT = 640;
-  static readonly CANVAS_WIDTH = 960;
+  //Network Constants
+  static readonly PORT : number = 3000;
+  static readonly MAX_CLIENTS : number = 2;
+  static readonly SERVER_URL : string = `http://localhost:${GlobalConstants.PORT}`;
 
-  static readonly PLAYER_HEIGHT = 120;
-  static readonly PLAYER_WIDTH = 120;
+  //Grid Constants
+  static readonly GRID_HEIGHT : number = 20; //Blocks
+  static readonly GRID_WIDTH : number = 30; //Blocks
+  static readonly BLOCK_HEIGHT : number = 32; //Pixels per Block
+  static readonly BLOCK_WIDTH : number = 32; // Pixels per Block
 
-  static readonly PLAYER_1_START_X_COORDS = 140;
-  static readonly PLAYER_2_START_X_COORDS = 820;
+  //Canvas Constants
+  static readonly CANVAS_HEIGHT : number = GlobalConstants.GRID_HEIGHT * GlobalConstants.BLOCK_HEIGHT; //640 Pixels
+  static readonly CANVAS_WIDTH : number = GlobalConstants.GRID_WIDTH * GlobalConstants.BLOCK_WIDTH; //960 Pixels
 
+  //Character Constants
+  static readonly PLAYER_HEIGHT : number = 120;
+  static readonly PLAYER_WIDTH : number  = 120;
+
+  static readonly PLAYER_1_START_X_COORDS : number = 140;
+  static readonly PLAYER_2_START_X_COORDS : number = 820;
+  
+  //Level Constants
   static readonly GROUND_HEIGHT = 80;
 
 }
