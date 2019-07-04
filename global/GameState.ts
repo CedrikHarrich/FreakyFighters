da9aa624066b4eff66ed1ef5e7aaef6f81e123e6
ru32puy <p.powers@campus.lmu.de>
2019-07-04 16:50:05 +0200

    removed unused code

--- global/GameState.ts
+++ global/GameState.ts
@@ -28,60 +28,6 @@ export class GameState {
   }
 }
 
-export class PlayerState0 {
-  readonly positionX: number;
-  readonly positionY: number;
-  readonly cursorX: number;
-  readonly cursorY: number;
-  readonly spriteNumber: number;
-  readonly id: number;
-  readonly isTakingAction: boolean
-  private actionState: ActionState;
-
-  constructor({positionX, positionY, cursorX, cursorY, spriteNumber, id, isTakingAction, actionState} : {positionX:number, positionY:number, cursorX: number, cursorY: number, spriteNumber:number, id:number, isTakingAction: boolean, actionState:ActionState}){
-    this.positionX = positionX;
-    this.positionY = positionY;
-    this.cursorX = cursorX;
-    this.cursorY = cursorY;
-    this.spriteNumber = spriteNumber;
-    this.id = id;
-    this.isTakingAction = isTakingAction;
-    this.actionState = actionState
-  }
-
-  getPositionX(){
-    return this.positionX;
-  }
-
-  getPositionY(){
-    return this.positionY;
-  }
-
-  getCursorX(){
-    return this.cursorX;
-  }
-
-  getCursorY(){
-    return this.cursorY;
-  }
-
-  getSpriteNumber(){
-    return this.spriteNumber;
-  }
-
-  getId(){
-    return this.id;
-  }
-
-  getActionState(){
-    return this.actionState;
-  }
-
-  setActionState(actionState: ActionState){
-    this.actionState = actionState;
-  }
-}
-
 export class PlayerState extends Player {
   private spriteNumber: number;
   private actionState: ActionState;
